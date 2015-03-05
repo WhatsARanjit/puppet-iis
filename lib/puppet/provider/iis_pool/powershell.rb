b105cdce7b45c4e0cc0dda79b5ed4b3df18a082d
WhatsARanjit <whatsaranjit@gmail.com>
2015-03-05 12:29:56 -0500

    Added refresh for iis_pool

--- lib/puppet/provider/iis_pool/powershell.rb
+++ lib/puppet/provider/iis_pool/powershell.rb
@@ -20,12 +20,13 @@ Puppet::Type.type(:iis_pool).provide(:powershell, :parent => Puppet::Provider::I
   end
 
   def self.instances
-    inst_cmd ='Import-Module WebAdministration; ls IIS:\\AppPools | Select Name | ConvertTo-JSON'
+    inst_cmd ='Import-Module WebAdministration; ls IIS:\\AppPools | Select Name, State | ConvertTo-JSON'
     pool_names = JSON.parse(run(inst_cmd))
     pool_names.collect do |pool|
       pool_cmd                  = "Import-Module WebAdministration; Get-ItemProperty \"IIS:\\\\AppPools\\#{pool['name']}\""
       pool_hash                 = {}
       pool_hash[:name]          = pool['name']
+      pool_hash[:state]         = pool['state']
       pool_hash[:enable_32_bit] = run("#{pool_cmd} enable32BitAppOnWin64.value").rstrip.downcase.to_sym
       pool_hash[:runtime]       = run("#{pool_cmd} managedRuntimeVersion.value").rstrip.gsub(/^v/, '')
       pool_hash[:pipeline]      = pipelines[run("#{pool_cmd} managedPipelineMode.value").rstrip.to_i]
@@ -80,4 +81,34 @@ Puppet::Type.type(:iis_pool).provide(:powershell, :parent => Puppet::Provider::I
       @property_hash[property] = value
     end
   end
+
+  def restart
+    inst_cmd = "Import-Module WebAdministration; Restart-WebAppPool -Name \"#{@resource[:name]}\""
+    resp = Puppet::Type::Iis_pool::ProviderPowershell.run(inst_cmd)
+    fail(resp) if resp.length > 0
+  end
+  
+  def enabled?
+    inst_cmd = "Import-Module WebAdministration; (Get-WebAppPoolState -Name \"#{@resource[:name]}\").value"
+    resp = Puppet::Type::Iis_pool::ProviderPowershell.run(inst_cmd).rstrip
+    if resp == 'Started'
+      return true
+    else
+      return false
+    end
+  end
+
+  def state=(value)
+    binding.pry
+    if value == :Started
+      inst_cmd = 'Start-WebAppPool'
+    else
+      inst_cmd = 'Stop-WebAppPool'
+    end
+    inst_cmd += " -Name \"#{@property_hash[:name]}\""
+    resp = Puppet::Type::Iis_pool::ProviderPowershell.run(inst_cmd)
+    fail(resp) if resp.length > 0
+    @property_hash[:state] = value
+  end
+
 end
