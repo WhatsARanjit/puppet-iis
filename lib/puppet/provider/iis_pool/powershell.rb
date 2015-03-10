e4ecd7b45c01467a063df0abbcdc9e89500599d1
WhatsARanjit <whatsaranjit@gmail.com>
2015-03-10 06:31:16 -0400

    Simplified instances to 1 PSH call

--- lib/puppet/provider/iis_pool/powershell.rb
+++ lib/puppet/provider/iis_pool/powershell.rb
@@ -27,16 +27,23 @@ Puppet::Type.type(:iis_pool).provide(:powershell, :parent => Puppet::Provider::I
   end
 
   def self.instances
-    inst_cmd ='Import-Module WebAdministration; ls IIS:\\AppPools | Select Name, State | ConvertTo-JSON'
-    pool_names = JSON.parse(run(inst_cmd))
+    inst_cmd = [
+      'Import-Module WebAdministration;',
+      '$pools=(ls "IIS:\\AppPools");',
+      '@(ForEach ($pool in $pools)',
+      '{ $p=($pool.name);',
+      'Get-ItemProperty "IIS:\\AppPools\$p" |',
+      'Select Name,State,enable32BitAppOnWin64,managedRuntimeVersion,managedPipelineMode }) |',
+      'ConvertTo-JSON'
+    ]
+    pool_names = JSON.parse(run(inst_cmd.join))
     pool_names.collect do |pool|
-      pool_cmd                  = "Import-Module WebAdministration; Get-ItemProperty \"IIS:\\\\AppPools\\#{pool['name']}\""
       pool_hash                 = {}
       pool_hash[:name]          = pool['name']
       pool_hash[:state]         = pool['state']
-      pool_hash[:enable_32_bit] = run("#{pool_cmd} enable32BitAppOnWin64.value").rstrip.downcase.to_sym
-      pool_hash[:runtime]       = run("#{pool_cmd} managedRuntimeVersion.value").rstrip.gsub(/^v/, '')
-      pool_hash[:pipeline]      = pipelines[run("#{pool_cmd} managedPipelineMode.value").rstrip.to_i]
+      pool_hash[:enable_32_bit] = pool['enable32BitAppOnWin64']
+      pool_hash[:runtime]       = pool['managedRuntimeVersion']
+      pool_hash[:pipeline]      = pool['managedPipelineMode']
       pool_hash[:ensure]        = :present
       new(pool_hash)
     end
