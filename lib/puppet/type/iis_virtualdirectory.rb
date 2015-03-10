1a547f7667aa731bebe09d2c5f1c65bd6e609e2a
WhatsARanjit <whatsaranjit@gmail.com>
2015-03-10 11:44:20 -0400

    Added virtual directory support

---
+++ lib/puppet/type/iis_virtualdirectory.rb
@@ -0,0 +1,31 @@
+require 'pry'
+Puppet::Type.newtype(:iis_virtualdirectory) do
+  desc 'The iis_virtualdirectory type creates and manages IIS virtual directories'
+  ensurable
+
+  newparam(:name, :namevar => true) do
+    desc 'This is the name of the virtual directory'
+    validate do |value|
+      fail("#{value} is not a valid virtual directory name") unless value =~ /^[a-zA-Z0-9\-\_\/\s]+$/
+    end
+  end
+
+  newproperty(:path) do
+    desc 'Path to the web site folder'
+    validate do |value|
+      fail("File paths must be fully qualified, not '#{value}'") unless value =~ /^.:\// or value =~ /^\/\/[^\/]+\/[^\/]+/
+    end
+  end
+
+  newproperty(:site) do
+    desc 'The site in which this virtual directory exists'
+    validate do |value|
+      fail('Site is read-only attribute.  To change site, remove and create a new virtual directory')
+    end
+  end
+
+  autorequire(:iis_site) do
+    self[:site] if @parameters.include? :site
+  end
+
+end
