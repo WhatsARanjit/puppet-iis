48b537494837dcfc4c7fcd5e920dd12d764fda7c
WhatsARanjit <whatsaranjit@gmail.com>
2015-03-11 09:45:48 -0400

    First go at applications

---
+++ lib/puppet/provider/iis_application/powershell.rb
@@ -0,0 +1,90 @@
+require 'puppet/provider/iispowershell'
+require 'json'
+require 'pry'
+
+Puppet::Type.type(:iis_application).provide(:powershell, :parent => Puppet::Provider::Iispowershell) do
+
+  def initialize(value={})
+    super(value)
+    @property_flush = {
+      'appattrs' => {},
+    }
+  end
+
+  def self.instances
+    inst_cmd = 'Import-Module WebAdministration; Get-WebApplication | Select path, physicalPath, applicationPool, ItemXPath | ConvertTo-JSON'
+    app_names = JSON.parse(run(inst_cmd))
+    app_names.collect do |app|
+      app_hash            = {}
+      app_hash[:name]     = app['path'].gsub(/^\//, '')
+      app_hash[:path]     = app['PhysicalPath']
+      app_hash[:app_pool] = app['applicationPool']
+      app_hash[:site]     = app['ItemXPath'].match(/@name='([a-z0-9_\ ]+)'/i)[1]
+      app_hash[:ensure]   = :present
+      new(app_hash)
+    end
+  end
+
+  def self.prefetch(resources)
+    apps = instances
+    resources.keys.each do |app|
+      if provider = apps.find{ |a| a.name == app }
+        resources[app].provider = provider
+      end
+    end
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present
+  end
+
+  mk_resource_methods
+
+  def create
+    inst_cmd = [
+      'Import-Module WebAdministration; ',
+      "New-WebApplication -Name \"#{@resource[:name]}\"",
+      "-PhysicalPath \"#{@resource[:path]}\"",
+      "-Site \"#{@resource[:site]}\"",
+      "-ApplicationPool \"#{@resource[:app_pool]}\"",
+      '-Force'
+    ]
+    resp = Puppet::Type::Iis_application::ProviderPowershell.run(inst_cmd.join(' '))
+
+    @resource.original_parameters.each_key do |k|
+      @property_hash[k] = @resource[k]
+    end
+
+    exists? ? (return true) : (return false)
+  end
+
+  def destroy
+    inst_cmd = [
+      'Import-Module WebAdministration; ',
+      'Remove-Item',
+      "\"IIS:\\Sites\\#{@property_hash[:site]}\\#{@property_hash[:name]}\"",
+      '-Force -Recurse'
+    ]
+    resp = Puppet::Type::Iis_application::ProviderPowershell.run(inst_cmd.join(' '))
+    fail(resp) if resp.length > 0
+
+    @property_hash.clear
+    exists? ? (return false) : (return true)
+  end
+
+  def path=(value)
+    @property_flush['appattrs']['physicalPath'] = value
+    @property_hash['path'] = value
+  end
+
+  def flush
+    command_array = []
+    command_array << "Import-Module WebAdministration"
+    @property_flush['appattrs'].each do |appattr,value|
+      command_array << "Set-ItemProperty \"IIS:\\\\Sites\\#{@property_hash[:site]}\\#{@property_hash[:name]}\" #{appattr} #{value}"
+    end
+    resp = Puppet::Type::Iis_application::ProviderPowershell.run(command_array.join('; '))
+    fail(resp) if resp.length > 0
+  end
+
+end
